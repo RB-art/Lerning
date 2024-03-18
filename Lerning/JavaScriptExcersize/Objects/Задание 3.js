7b3c009d3cc2328066964813b5ca4f92ea3878e5
RB-art <98849158+RB-art@users.noreply.github.com>
2024-03-18 13:14:29 +0200

    Revert "starts"
    This reverts commit f01d3208c21bd7e2a557dec4d7418cfad417b817.

--- Lerning/JavaScriptExcersize/Objects/Задание 3.js
+++
@@ -1,3 +0,0 @@
-// Написать функцию, которая создает пустой объект, но без прототипа.
-const noPrototypeFunc = () => {return Object.create(null)};
-console.log(noPrototypeFunc()); 
--- Lerning/JavaScriptExcersize/Задание 3.js
+++
@@ -1,7 +0,0 @@
-//Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH".
-let string = "Hello"
-let splitstring = string.split("");
-let reverseAray = splitstring.reverse();
-let joinAray = reverseAray.join("");
-console.log(joinAray)
-
